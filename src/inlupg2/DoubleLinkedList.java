32dbb26fa845451c2646a2c6dd976f3d6890097d
Dennis <d.wildmark@gmail.com>
2015-03-17 09:01:30 +0100

    progress

--- src/inlupg2/DoubleLinkedList.java
+++ src/inlupg2/DoubleLinkedList.java
@@ -2,24 +2,26 @@ package inlupg2;
 
 public class DoubleLinkedList<E> {
 	private ObjectNode<E> first, last;
-	private int size;
 	
-	public DoubleLinkedList(ObjectNode<E> first, ObjectNode<E> last) {
+	public DoubleLinkedList(ObjectNode<E> first) {
 		this.first = first;
-		this.last = last;
+		this.last = first;
 		this.first.setNext(this.last);
-		if(this.last != null) {
-			this.last.setPrevious(this.first);
-		}
-		size = 1;
+		this.last.setPrevious(this.first);
 	}
 	
 	public int size() {
-		return size;
+		ObjectNode<E> temp = first;
+		int count = 1;
+		while(temp.getNext() != null) {
+			temp = temp.getNext();
+			count++;
+		}
+		return count;
 	}
 	
 	public ObjectNode<E> get(int index) {
-		if(size >= index + 1) {
+		if(size() > index) {
 			ObjectNode<E> node = first;
 			for(int i = 0; i < index; i++) {
 				node = node.getNext();
@@ -31,10 +33,32 @@ public class DoubleLinkedList<E> {
 	}
 	
 	public void add(E object, int index) {
-		if(index < size && index > 0) {
-			ObjectNode<E> objectNode = new ObjectNode<E>(object, get(index + 1), get(index - 1));
-			size++;
+		ObjectNode<E> prev, next;
+		if(index < size() - 1 && index > 0) {
+			prev = get(index - 1);
+			next = get(index + 1);
+			ObjectNode<E> objectNode = new ObjectNode<E>(object, next, prev);
+			prev.setNext(objectNode);
+			if(next != null) {
+				next.setPrevious(objectNode);
+			}
+		} else if(index == 0) {
+			addFirst(object);
+		} else if(index == size()) {
+			addLast(object);
 		}
+	}
+	
+	public void addFirst(E object) {
+		ObjectNode<E> objectNode = new ObjectNode<E>(object, first, null);
+		first.setPrevious(objectNode);
+		first = objectNode;
+	}
+	
+	public void addLast(E object) {
+		ObjectNode<E> objectNode = new ObjectNode<E>(object, null, last);
+		last.setNext(objectNode);
+		last = objectNode;
 	}
 	
 	public void remove(int index) {
--- src/inlupg2/MainDLL.java
+++ src/inlupg2/MainDLL.java
@@ -2,9 +2,13 @@ package inlupg2;
 
 public class MainDLL {
 	public static void main(String[] args) {
-		ObjectNode<String> node1 = new ObjectNode("Hej", null, null);
-		DoubleLinkedList<String> list = new DoubleLinkedList(node1, null);
-		list.add("jag", 1);
-		System.out.println(list.get(1).getData());
+		ObjectNode<String> node1 = new ObjectNode<String>("Hej", null, null);
+		DoubleLinkedList<String> list = new DoubleLinkedList<String>(node1);
+		list.addFirst("jag");
+		list.addFirst("heter");
+		list.addLast("Dennis");
+		list.add("index 0", 3);
+		System.out.println(list.get(0).getData());
+		System.out.println(list.size());
 	}
 }
